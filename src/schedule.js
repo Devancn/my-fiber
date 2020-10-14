1f29c868bbadf7415de7f61f1f16358d6ef3c678
dingxiaohuan <wydingxiaohuan@163.com>
2020-10-14 21:44:48 +0800

    html元素情况下的增删改

--- src/schedule.js
+++ src/schedule.js
@@ -11,13 +11,33 @@ import {
   TAG_TEXT,
   TAG_HOST,
   PLACEMENT,
+  DELETION,
+  UPDATE,
 } from "./constants";
 import { setProps } from "./utils";
 let nextUnitOfWork = null; // 下一个工作单元
-let workInProgressRoot = null; // RootFiber应用的根
+let workInProgressRoot = null; // 正在渲染的RootFiber应用的根节点
+let currentRoot = null; // 上一次的workInProgressRoot（有值代表页面已经渲染过）
+let deletions = []; // 记录需要删除的effect list
 export function scheduleRoot(rootFiber) {
-  workInProgressRoot = rootFiber;
-  nextUnitOfWork = rootFiber;
+  // 此时页面已经更新渲染过一次
+  if (currentRoot && currentRoot.alternate) {
+    workInProgressRoot = currentRoot.alternate; // 把上上次的渲染rootFiber作为此次的workInProgressRoot
+    workInProgressRoot.props = rootFiber.props;
+    workInProgressRoot.alternate = currentRoot;
+    // 此时页面已经渲染过一次
+  } else if (currentRoot) {
+    // 新的渲染rootFiber的alternate属性指向上次已经渲染过得rootFiber节点
+    rootFiber.alternate = currentRoot;
+    // 把workInProgressRoot指向当前的rootFiber节点
+    workInProgressRoot = rootFiber;
+  } else {
+    workInProgressRoot = rootFiber;
+  }
+  workInProgressRoot.firstEffect = null;
+  workInProgressRoot.lastEffect = null;
+  workInProgressRoot.nextEffect = null;
+  nextUnitOfWork = workInProgressRoot;
 }
 
 // 深度遍历转换成fiber节点，知道叶子节点
@@ -90,7 +110,7 @@ function beginWork(currentFiber) {
  * 主要两件事：
  * 1. 创建当前fiber对应DOM元素
  * 2. 根据当前fiber的props的children(此时的children为react元素)创建fiber节点，并把当前fiber节点指向children元素的第一个react元素转换之后的fiber节点，children中的fiber节点使用sibling相连
- * @param {object} currentFiber fiber元素 
+ * @param {object} currentFiber fiber元素
  */
 function updateHost(currentFiber) {
   if (!currentFiber.stateNode) {
@@ -125,33 +145,67 @@ function updateHostRoot(currentFiber) {
   reconcileChildren(currentFiber, newChildren);
 }
 
+/**
+ * 调和子react元素转换成fiber节点，并把当前fiber节点的child为子fiber列表中的第一个，子fiber之间用sibling指针连接起来
+ * @param {object} currentFiber 当前fiber节点
+ * @param {object} newChildren 当前fiber节点对应的子react元素节点
+ */
 function reconcileChildren(currentFiber, newChildren) {
   let newChildIndex = 0; // newChildren节点的索引
+  let oldFiber = currentFiber.alternate && currentFiber.alternate.child;
   let prevSibling; // 上一个子fiber
-  while (newChildIndex < newChildren.length) {
+  while (newChildIndex < newChildren.length || oldFiber) {
+    // 如果有新增元素则会走或逻辑
     let newChild = newChildren[newChildIndex]; // 取出当前react元素
+    let newFiber;
+    const sameType = oldFiber && newChild && oldFiber.type === newChild.type;
     let tag;
-    if (newChild.type === ELEMENT_TEXT) {
+    if (newChild && newChild.type === ELEMENT_TEXT) {
       tag = TAG_TEXT;
-    } else if (typeof newChild.type === "string") {
+    } else if (newChild && typeof newChild.type === "string") {
       tag = TAG_HOST;
     }
-    let newFiber = {
-      tag, // 当前react元素对应的fiber类型
-      type: newChild.type, // react元素的type
-      props: newChild.props, // react元素的的props
-      stateNode: null, // 真实DOM
-      return: currentFiber, // 父fiber
-      effectTag: PLACEMENT, // 副作用标识（增加、删除、插入）
-      nextEffect: null, // 值也是单链表
-    };
-    if (newChildIndex === 0) {
-      // 当前Fiber节点的child指向children Fiber的第一个
-      currentFiber.child = newFiber;
+    if (sameType) {
+      // 老fiber的type与新react元素的type一致，则复用老的DOM节点，更新即可
+      newFiber = {
+        tag: oldFiber.tag, // 当前react元素对应的fiber类型
+        type: oldFiber.type, // react元素的type
+        props: newChild.props, // react元素的的props
+        stateNode: oldFiber.stateNode, // 真实DOM
+        alternate: oldFiber, // 新的fiber节点指向老的fiber节点
+        return: currentFiber, // 父fiber
+        effectTag: UPDATE, // 副作用标识（增加、删除、插入）
+        nextEffect: null, // 值也是单链表
+      };
     } else {
-      prevSibling.sibling = newFiber;
+      if (newChild) {
+        newFiber = {
+          tag, // 当前react元素对应的fiber类型
+          type: newChild.type, // react元素的type
+          props: newChild.props, // react元素的的props
+          stateNode: null, // 真实DOM
+          return: currentFiber, // 父fiber
+          effectTag: PLACEMENT, // 副作用标识（增加、删除、插入）
+          nextEffect: null, // 值也是单链表
+        };
+      }
+      if (oldFiber) {
+        oldFiber.effectTag = DELETION;
+        deletions.push(oldFiber);
+      }
     }
-    prevSibling = newFiber;
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+    if (newFiber) {
+      if (newChildIndex === 0) {
+        // 当前Fiber节点的child指向children Fiber的第一个
+        currentFiber.child = newFiber;
+      } else {
+        prevSibling.sibling = newFiber;
+      }
+      prevSibling = newFiber;
+    }
     newChildIndex++;
   }
 }
@@ -171,22 +225,42 @@ function workLoop(deadline) {
 }
 
 function commitRoot() {
+  // 把记录在deletions中的effect list对应的DOM节点删除
+  deletions.forEach(commitWork);
+
   let currentFiber = workInProgressRoot.firstEffect;
   console.log(workInProgressRoot);
-  debugger
   while (currentFiber) {
     commitWork(currentFiber);
     currentFiber = currentFiber.nextEffect;
   }
+  deletions.length = 0;
+  currentRoot = workInProgressRoot; // 把当前已经渲染成功之后的workInProgressRoot用currentRoot保存起来
   workInProgressRoot = null;
 }
 
 function commitWork(currentFiber) {
   if (!currentFiber) return;
   let returnFiber = currentFiber.return;
-  let returnDOM = returnFiber.stateNode;
+  let DOMReturn = returnFiber.stateNode;
   if (currentFiber.effectTag === PLACEMENT) {
-    returnDOM.appendChild(currentFiber.stateNode);
+    // 新增节点
+    DOMReturn.appendChild(currentFiber.stateNode);
+  } else if (currentFiber.effectTag === DELETION) {
+    // 删除节点(此时DOMReturn是DOM元素)
+    DOMReturn.removeChild(currentFiber.stateNode);
+  } else if (currentFiber.effectTag === UPDATE) {
+    if (currentFiber.type === ELEMENT_TEXT) {
+      if (currentFiber.alternate.props.text !== currentFiber.props.text) {
+        currentFiber.stateNode.textContent = currentFiber.props.text;
+      }
+    }
+  } else {
+    updateDOM(
+      currentFiber.stateNode,
+      currentFiber.alternate.props,
+      currentFiber.props
+    );
   }
   currentFiber.effectTag = null;
 }
